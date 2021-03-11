a415f6060bf5d7a385c35b03d17e2756d3098312
Kevin Robayna <me@kevinrobayna.com>
2021-03-11 14:17:08 +0000

    remove --wait flag

--- hello_world.go
+++ hello_world.go
@@ -1,9 +1,11 @@
 package main
+
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"encoding/json"
 )
+
 func main() {
 	handler := http.NewServeMux()
 	///we create a new router to expose our api to our users
@@ -16,7 +18,7 @@ func SayHello(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	var response = make(map[string]string)
-    response[`message`] = `Hello world`
+	response[`message`] = `Hello world`
 
 	fmt.Println("Hello world")
 
