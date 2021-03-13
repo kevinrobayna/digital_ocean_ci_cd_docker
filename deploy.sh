ab885c01ec5bd7fcdf23ba35368331c12a98932c
Kevin Robayna <me@kevinrobayna.com>
2021-03-13 11:43:17 +0000

    add step to deploy image

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-username=$(wc -l < $1)
-password=$(wc -l < $2)
-
-echo "$password" | docker login registry.digitalocean.com --username $username --password-stdin
-
-docker run -d -p 80:8080 --name helloworld do-kevinrobayna-docker-registry/helloworld:latest
